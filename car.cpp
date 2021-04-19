3b886c245aad0ae538c8679ba43b28229e2280ce
Andrew <x.and.i.rew.e@gmail.com>
2021-04-20 02:54:30 +1000

    completed project

---
+++ car.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  // heard C++ gud for beep boop
+  if (!car_move()) car_move();
+  // should be gud enough i dont see how it can be improved
+  // feel free to send in some pull requests but i dont think it can beat this
+}
