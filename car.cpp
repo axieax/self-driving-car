cc89842dbe72b11ac2338fba2ead7f664201af61
Andrew Xie <62098008+axieax@users.noreply.github.com>
2021-12-23 14:57:36 +1100

    feat: engine sounds

--- car.cpp
+++ car.cpp
@@ -6,4 +6,6 @@ int main() {
   // should be gud enough i dont see how it can be improved
   // feel free to send in some pull requests but i dont think it can beat this
   if (car_crash()) die();
+  // get out me mom's car vroom vroom
+  for (;;) std::cout << "Vroom Vroom" << std::endl;
 }
