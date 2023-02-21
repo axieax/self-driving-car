92c086e1ac0864a0d1155138904508cedd258411
Andrew Xie <62098008+axieax@users.noreply.github.com>
2023-02-21 14:35:33 +1100

    feat: improve car safety
    Using state of the art technology

--- car.cpp
+++ car.cpp
@@ -6,6 +6,7 @@ int main() {
   if (!car_move()) car_move();
   // should be gud enough i dont see how it can be improved
   // feel free to send in some pull requests but i dont think it can beat this
+  if (going_to_crash()) dont();
   if (car_crash()) die();
   // get out me mom's car vroom vroom
   for (;;) std::cout << "Vroom Vroom" << std::endl;
